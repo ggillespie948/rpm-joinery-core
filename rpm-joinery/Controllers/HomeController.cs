1fab0795d9f55cccfc884de69910c1bf5e65510f
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-07-31 12:56:49 +0100

    add home controller shell

--- rpm-joinery/Controllers/HomeController.cs
+++ rpm-joinery/Controllers/HomeController.cs
@@ -12,13 +12,55 @@ namespace rpm_joinery.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            //Set SEO keywords, description, and other meta data in view bag so can be changed dynamically in layout
+            ViewBag.Title = "RPM Joinery and      Maintenance | Dundee Joinery Services";
+            ViewBag.MetaDescription = "RPM Joinery and Maintenance provide top quality joinery and maintenance services to Dundee, Angus and Fife, all at a great price.";
+            ViewBag.MetaKeywords = "RPM joinery maintenance, dundee joiner, dundee joinery, lochee joiner, broughty ferry joiner, rpm dundee, kitchen fitting dundee, decking dundee, reliable joiner dundee, flat renovation dundee, bathroom fitting dundee, windows, doors, property management";
+
+
+            // Retrieve the 3 most recent projects from the data base and display in the preview of the index home page 
+            List<Project> projects = new List<Project>();
+            projects.Reverse();
+            List<Project> recentProjects = new List<Project>();
+            int projectCounter = 0;         
+
+            foreach (Project proj in projects)
+            {
+                recentProjects.Add(proj);
+                projectCounter++;
+
+                if (projectCounter > 2)
+                    break;
+            }
+
+            return View(recentProjects);
         }
 
         public IActionResult Privacy()
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult Cookies()
+        {
+            return View();
+        }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
