6460645c074e7dbff4600f513b7fe00afd68b6a1
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-07-31 12:57:42 +0100

    add project, project type models

---
+++ rpm-joinery/Models/Projects/Project.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rpm_joinery.Models
+{
+    public class Project
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ProjectTypeId { get; set; } 
+        public ProjectType ProjectType { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+        public Guid CreatedByUserId { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ServicesProvided { get; set; }
+
+        public List<string> Tags { get; set; }
+        public List<string> ImageFilePaths { get; set; }
+        public List<string> ImageDescriptions { get; set; }
+    }
+}
---
+++ rpm-joinery/Models/Projects/ProjectType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rpm_joinery.Models
+{
+    public class ProjectType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
