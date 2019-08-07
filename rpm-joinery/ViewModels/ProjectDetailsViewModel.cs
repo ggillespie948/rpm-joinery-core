6433554b3b69b488d4f2e57c2c08d529480894f4
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2019-08-07 23:45:59 +0100

    index + project details vm

---
+++ rpm-joinery/ViewModels/ProjectDetailsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using rpm_joinery.Models;
+using rpm_joinery.Models.Projects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rpm_joinery.ViewModels
+{
+    public class ProjectDetailsViewModel
+    {
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+        public List<Tag> BrowseTags { get; set; }
+
+        public int NextProjectId { get; set; }
+        public string NextProjectTile { get; set; }
+
+        public int PreviousProjectId { get; set; }
+        public string PreviousProjectTile { get; set; }
+    }
+}
